c199041b23036cfd435a0fa25524e6e03dbe7d70
tyneon <tyanales+github@gmail.com>
2023-08-13 17:28:32 +0400

    Added task birthday_map_2

--- solution.dart
+++ solution.dart
@@ -1,16 +1,32 @@
-// Это вариант решения задачи -- работающий код!
-void func() {
-  for (int i = 0; i < 5; i++) {
-    print('hello ${i + 1}');
+(bool, List<String>) check(
+  dynamic birthdays,
+  String month,
+  Function participantSolution,
+  Function jurySolution,
+) {
+  try {
+    final part = participantSolution(birthdays, month);
+    final jury = jurySolution(birthdays, month);
+    
+    // Тут я использую метод containsAll, который проверяет,
+    // все ли объекты второго сета содержатся в первом.
+    // Если множество A включает B и B включает A, они совпадают.
+    if (!part.containsAll(jury) || !jury.containsAll(part)) {
+      return (false, ["$part != $jury"]);
+    }
+    
+    // Используя встренные методы сетов,
+    // можно было также проверить, что union этих двух сетов
+    // совпадает по размеру с обоими сетами.
+    
+    // Также можно было указать в начале файла
+    // import "package:collection/collection.dart";
+    // и использовать SetEquality() для сравнения сетов.
+  } catch (e) {
+    return (
+      false,
+      ['Возникло исключение ${e.runtimeType}'],
+    );
   }
+  return (true, []);
 }
-
-// Вы всегда можете посмотреть solution для задания,
-// и никто об этом не узнает!
-// Но всегда лучше сначала постараться решить задание самостоятельно.
-
-// То, что в solution указан именно этот код, не значит,
-// что это единственно верное решение. Например,
-// функцию func можно оставить с возвращаемым значением int,
-// просто вернуть какое-то числовое значение в конце.
-// А переменную i можно также объявить до цикла for.
--- test.dart
+++ test.dart
@@ -1,13 +1,130 @@
-// Ваше любопытство вознаграждено! Вы нашли код чекера для задания.
-// Это главная функция программы, дла которой вы пишете модуль.
-// Тут содержатся все тесты и логика проверки вашего решения.
-// Иногда тут также будут подключаться библиотеки и объявляться константы,
-// которые сможет использовать ваш код.
-void main() {
-  try {
-    func();
-  } catch (e) {
-    _result(false, ["Исключение при попытке вызвать функцию func: ${e.runtimeType}"]);
+
+Set<String> juryGetAllBirthdaysForMonth(dynamic birthdays, String month) {
+  final data = birthdays as Map<String, Map<dynamic, dynamic>>;
+  if (data[month]!.isEmpty) {
+    return <String>{};
   }
-  _result(true, []);
+  var results = <String>{};
+  for (final item in data[month]!.values) {
+    results.addAll(item);
+  }
+  return results;
+}
+
+Set<String> wrongGetAllBirthdaysForMonth(dynamic birthdays, String month) {
+  final data = birthdays as Map<String, Map<dynamic, dynamic>>;
+  if (data[month]!.isEmpty) {
+    return <String>{};
+  }
+  var results = <String>{};
+  for (final item in data[month]!.values) {
+    results.addAll(item);
+  }
+  var falseResults = <String>{};
+  for (int i = 0; i < results.length; i++) {
+    falseResults.add(i.toString());
+  }
+  return falseResults;
+}
+
+void main() {
+  const _birthdays = {
+    "Jan": {
+      10: {"Anastasia"},
+      15: {"Anton"},
+    },
+    "Feb": {
+      4: {"Alexei"},
+      22: {"Nancy"},
+    },
+    "Mar": {
+      12: {"Val"},
+    },
+    "Apr": {
+      28: {"Nicola"},
+      25: {"Sergei"},
+    },
+    "May": {
+      8: {"Margo"},
+      10: {"Alex"},
+      29: {"Russ"},
+    },
+    "Jun": {
+      24: {"Nik"},
+      25: {"Kira", "Sandra"},
+    },
+    "Jul": {
+      14: {"Love"},
+      19: {"Dmytro"},
+      24: {"Gleb"},
+    },
+    "Aug": {
+      1: {"Elija"},
+      19: {"Ty", "Lewis"},
+      28: {"Tim"},
+    },
+    "Sep": {},
+    "Oct": {
+      16: {"Miguel"},
+      23: {"Halina"},
+    },
+    "Nov": {},
+    "Dec": {
+      1: {"Svyat"},
+      18: {"Marina"},
+    },
+  };
+
+  for (final test in _birthdays.keys) {
+    try {
+      var (bool checkResult, List<String> messages) = check(
+        _birthdays,
+        test,
+        juryGetAllBirthdaysForMonth,
+        juryGetAllBirthdaysForMonth,
+      );
+      if (!checkResult) {
+        _result(false, [
+          "Ваш чекер выдаёт ложноотрицательные вердикты:",
+          ...messages,
+        ]);
+        return;
+      }
+
+      if (_birthdays[test]!.isNotEmpty) {
+
+        (checkResult, messages) = check(
+          _birthdays,
+          test,
+          wrongGetAllBirthdaysForMonth,
+          juryGetAllBirthdaysForMonth,
+        );
+        if (checkResult) {
+          _result(false, [
+            "Ваш чекер выдаёт ложноположительные вердикты",
+            ...messages,
+          ]);
+          return;
+        }
+        (checkResult, messages) = check(
+          _birthdays,
+          test,
+          () => <String>{},
+          juryGetAllBirthdaysForMonth,
+        );
+        if (checkResult) {
+          _result(false, [
+            "Ваш чекер считает пустой сет равным непустому?.. :",
+            ...messages,
+          ]);
+          return;
+        }
+      }
+    } catch (e) {
+      _result(false,
+          ['Исключение при попытке вызвать функцию check: ${e.runtimeType}']);
+      return;
+    }
+  }
+  _result(true);
 }
