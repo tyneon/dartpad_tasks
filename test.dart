3b0897d7c35ae9c3306696fb289ddbff95d2ec71
tyneon <tyanales+github@gmail.com>
2023-08-13 17:19:42 +0400

    Add task to_binary

--- test.dart
+++ test.dart
@@ -1,13 +1,27 @@
-// Ваше любопытство вознаграждено! Вы нашли код чекера для задания.
-// Это главная функция программы, дла которой вы пишете модуль.
-// Тут содержатся все тесты и логика проверки вашего решения.
-// Иногда тут также будут подключаться библиотеки и объявляться константы,
-// которые сможет использовать ваш код.
-void main() {
-  try {
-    func();
-  } catch (e) {
-    _result(false, ["Исключение при попытке вызвать функцию func: ${e.runtimeType}"]);
+int toDecimal(String binary) {
+  int result = 0;
+  for (int i = 0; i < binary.length; i++) {
+    result = result * 2 + ((binary[i] == '1') ? 1 : 0);
   }
-  _result(true, []);
+  return result;
+}
+
+void main() {
+  final testCases = <int>[4, 10, 7, 518, 365, 256, 1, 0];
+  for (final test in testCases) {
+    try {
+      final String part = toBinary(test);
+      final int back = toDecimal(part);
+      if (test != back) {
+        _result(false,
+            ['Функция выводит что-то не то: $test --> $part --> $back.']);
+      }
+    } catch (e) {
+      _result(false, [
+        'Исключение при попытке вызвать функцию toBinary: ${e.runtimeType}'
+      ]);
+      return;
+    }
+  }
+  _result(true);
 }
