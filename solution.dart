bebca791c674473fed6c95dbf8fd1934b0b4902d
tyneon <tyanales+github@gmail.com>
2023-09-24 21:23:15 +0400

    Add solution explanation

--- solution.dart
+++ solution.dart
@@ -7,3 +7,11 @@ class TestTaker {
 List<TestTaker> getTestTakers(List<String> names, int maxVariant) => names
     .map((name) => TestTaker(name, names.indexOf(name) % maxVariant + 1))
     .toList();
+/* Используем метод map, чтобы применить действие - создание объекта TestTaker -
+ * к каждому элементу списка names.
+ * 
+ * Используем метод indexOf, чтобы получить индекс имени в списке names,
+ * и из индекса вычисляем вариант с использовнием остатка от деления.
+ * 
+ * Используем метод toList, чтобы привести результат к типу List.
+ */
